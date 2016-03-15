573d7be2870782e83f4ebe5e5a2e1e35da5b868d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-03-15 10:30:07 +0100

    Tweet messages

---
+++ data/tweets.js
@@ -0,0 +1,10 @@
+/**
+ * Created by maurice on 3/15/2016.
+ */
+
+
+module.exports = [
+    "I just supported The React Newsletter on @ThunderclapIt // @MauriceDB http://thndr.me/hVfWCl",
+    "Awesome stuff: mobx-react-devtools shows which components are re-rendered during a change. https://t.co/TTc8qEqJtg",
+    ""
+];
---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by maurice on 3/15/2016.
+ */
+
+var Twitter = require('twitter');
+var tweets = require('./data/tweets');
+var interval = 1000 * 60 * 60 * 6;
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+function getTweet() {
+    var index = Math.floor(Math.random() * tweets.length);
+    return tweets[index];
+}
+
+function sendTweet(status) {
+
+    client.post('statuses/update',
+        {
+            status: status
+        },
+        function (error, tweet, response) {
+            if (error) throw error;
+            console.log(tweet);  // Tweet body.
+            //console.log(response);  // Raw response object.
+        });
+}
+
+
+setInterval(function () {
+    var tweet = getTweet();
+    if (tweet) {
+        console.log(tweet);
+        sendTweet(tweet);
+    }
+}, interval);
