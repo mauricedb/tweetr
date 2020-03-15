68381d42be1f178dcd138dda011da9a3a974dde3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-03-15 20:42:11 +0100

    google-spreadsheet => 3

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-var Twitter = require('twitter');
-var GoogleSpreadsheet = require("google-spreadsheet");
-
+var Twitter = require("twitter");
+var { GoogleSpreadsheet } = require("google-spreadsheet");
 
 var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-function getTweet(cb) {
-    var sheet = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+async function getTweet(cb) {
+  var doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+  await doc.useApiKey(process.env.GOOGLE_API_KEY);
 
-    var sheets = [1, 2];
-    var sheetIndex = Math.floor(Math.random() * sheets.length);
+  var sheets = [1, 2];
+  var sheetIndex = Math.floor(Math.random() * sheets.length);
 
-    sheet.getRows(sheets[sheetIndex], function (error, tweets) {
-        if (error) throw error;
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[sheetIndex];
 
-        console.log(sheets[sheetIndex], tweets.length);
+  const tweets = await sheet.getRows();
+  console.log("tweets", tweets.length);
 
-        var index = Math.floor(Math.random() * tweets.length);
-        var tweet = tweets[index];
-        cb(tweet);
-    });
+  var index = Math.floor(Math.random() * tweets.length);
+  var tweet = tweets[index];
+  cb(tweet);
 }
-
 
 function sendTweet(status) {
-    console.log(status);
+  console.log(status);
 
-    client.post('statuses/update', {
-            status: status
-        },
-        function (error, tweet, response) {
-            if (error) {
-                console.log(error);
-                throw error;
-            }
-        });
+  client.post(
+    "statuses/update",
+    {
+      status: status
+    },
+    function(error, tweet, response) {
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+    }
+  );
 }
 
-getTweet(function (tweet) {
-    if (tweet && tweet.tweet) {
-        var msg = tweet.tweet;
+getTweet(function(tweet) {
+  if (tweet && tweet.Tweet) {
+    var msg = tweet.Tweet;
 
-        if (tweet.by) {
-            msg += '. By ' + tweet.by;
-        }
-
-        if (tweet.link) {
-            msg += ' ' + tweet.link;
-        }
-
-        sendTweet(msg);
+    if (tweet.By) {
+      msg += ". By " + tweet.By;
     }
+
+    if (tweet.Link) {
+      msg += " " + tweet.Link;
+    }
+
+    sendTweet(msg);
+  }
 });
